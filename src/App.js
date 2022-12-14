337fdc76236b9a76b76df9676d8a286db9d41fdc
Yuldenys <yuldenis@gmail.com>
2022-12-14 23:40:48 +0200

    Add css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,25 @@ export default function App() {
       <div className="container">
         <h1>Weather App</h1>
         <WeatherSearch />
+        <footer>
+          <small>
+            <a
+              href="https://github.com/Yuldenys/react-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-source code
+            </a>
+            , by Yuliya Denysenko from
+            <a
+              href="https://shecodes.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              She Codes
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -12,25 +12,32 @@ export default function WeatherSearch() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>New York</h1>
+      <h2>New York</h2>
       <ul>
         <li>Wednesday 11:12</li>
         <li>Partly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Partly cloudy"
           />
-          6ºC
+
+          <span className="temperature">6</span>
+          <span className="unit">ºC</span>
         </div>
         <div className="col-6">
           <ul>
